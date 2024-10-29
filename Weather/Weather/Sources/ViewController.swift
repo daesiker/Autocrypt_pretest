74ef2f2fc0e6c940354421e33afb6ebeccf2ca89
Daesiker <eotlr680@naver.com>
2024-10-29 21:36:15 +0900

    [Init] : init xcode project
    xcode 프로젝트 생성 및 gitignore 파일 추가

---
+++ Weather/Weather/Sources/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Daesik Jun on 10/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .blue
+    }
+
+}
+
